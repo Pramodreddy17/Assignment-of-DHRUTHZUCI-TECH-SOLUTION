aac991a6c11c0b90edf1c39c0668039cddb8475b
Pramodreddy17 <49786679+Pramodreddy17@users.noreply.github.com>
2021-03-05 18:06:46 +0530

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -1,18 +1,10 @@
-x= int(input("Enter the size of first array: "))
-y= int(input("Enter the size of second array: "))
-list1=[]
-for i in range(0,x):
-    a=int(input())
-    list1.append(a)
-list2=[]
-for i in range(0,y):
-    a=int(input())
-    list2.append(a)
-b=set()
-for i in range(0,x):
-    b.add(list1[i])
-c=set()
-for i in range(0,y):
-    c.add(list2[i])
-intersection = b.intersection(c)
-print(intersection)
+from typing import List
+
+
+def intersection(nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
+
+
+arr1 = [1,2,2,1]
+arr2 = [2,2]
+print(intersection(arr1,arr2))
